b642fb21a2b8a0fd80449079ce6f859369e51054
ADSJohn <progjohn@outlook.com>
2025-08-01 20:49:54 -0300

    Codigo para contar quantidade de espiras

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,61 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define pinSensorHall 7
+#define pinRele 10
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const int BOBINAS = 4;
+const int ESPIRAS_PRIMARIO = 5;
+const int ESPIRAS_SECUNDARIO = 5;
+
+int bobina = 0;
+int contadorGiros = 0;
+bool sensorDesativado = false;
+bool bobinandoPrimario = true;
+
+void setup()
+{
+  pinMode(pinSensorHall, INPUT_PULLUP);
+  pinMode(pinRele, OUTPUT);
+  digitalWrite(pinRele, HIGH);
+  Serial.begin(9600);
+  Serial.println("Iniciando bobinagem...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  bool sensorHall = !digitalRead(pinSensorHall);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (sensorHall && !sensorDesativado && bobina < BOBINAS)
+  {
+    contadorGiros++;
+    Serial.print("Espira ");
+    Serial.print(contadorGiros);
+    Serial.println(bobinandoPrimario ? " do primário" : " do Secundário");
+
+    if (bobinandoPrimario && contadorGiros >= ESPIRAS_PRIMARIO)
+    {
+      contadorGiros = 0;
+      bobinandoPrimario = false;
+      Serial.println("Bobina do primário concluída.");
+    }
+    else if (!bobinandoPrimario && contadorGiros >= ESPIRAS_SECUNDARIO)
+    {
+      contadorGiros = 0;
+      bobinandoPrimario = true;
+      bobina++;
+      Serial.print("Bobina ");
+      Serial.print(bobina);
+      Serial.println(" concluída.");
+    }
+
+    if (bobina >= BOBINAS)
+    {
+      Serial.println("Todas as bobinas concluídas.");
+      digitalWrite(pinRele, LOW);
+      while (true)
+        ;
+    }
+  }
+
+  sensorDesativado = sensorHall;
 }
